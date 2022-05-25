f48543b15dd673f5b0556e260a694ada461d2582
ChrisP83 <cristian@homerlearning.com>
2022-05-25 12:52:07 -0400

    first commit, taking a break

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is:</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Cristian Petrescu</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Full-Stack Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>10+ years of experience building games and responsive websites.<br />
+          This portfolio is built from ground up using React and Tailwind.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-lg hover:bg-pink-600 hover:border-pink-500'>
+            View Work
+            <span className='group-hover:rotate-90 duration-200'>
+              <HiArrowNarrowRight className='ml-3' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
